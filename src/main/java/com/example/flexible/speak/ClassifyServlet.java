c95730fc11158eff6311f07773e67b7f39f73602
MagicoAshie <34609905+MagicoAshie@users.noreply.github.com>
2020-10-09 15:45:50 +0300

    Add files via upload

---
+++ src/main/java/com/example/flexible/speak/ClassifyServlet.java
@@ -0,0 +1,20 @@
+package com.example.flexible.speak;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ClassifyServlet extends HttpServlet {
+		@Override
+		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+			// TODO Auto-generated method stub
+			super.doGet(req, resp);
+			String name = req.getParameter("Transcript");
+			
+			resp.getWriter().println("<html>");
+			resp.getWriter().println("</html>");
+		}
+}
